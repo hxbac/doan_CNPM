56d89f9a009930a9211347b971838f20fb2139e2
TuanAnh <tuankw160202@gmail.com>
2023-10-30 21:48:32 +0700

    feat: [#12] Client-Show list menu

---
+++ app/View/Components/MenuComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Menu;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class MenuComponent extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $menus = Menu::get();
+        return view('components.menu-component',
+        ["menus" => $menus]
+        );
+    }
+}
